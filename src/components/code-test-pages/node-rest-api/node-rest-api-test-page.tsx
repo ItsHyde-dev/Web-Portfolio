830adcbb5181a19e375aac281a013ea3594a2fb8
Himanshu Joshi <himanshu6.joshi@ril.com>
2023-05-25 13:23:09 +0530

    added expanding div to rest api page

--- src/components/code-test-pages/node-rest-api/node-rest-api-test-page.tsx
+++ src/components/code-test-pages/node-rest-api/node-rest-api-test-page.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../../navbar/navbar';
 import styles from './node-rest-api-test.module.css'
-import animation from'../../../common/styles/animations.module.css'
+import animation from '../../../common/styles/animations.module.css'
+import position from '../../../common/styles/positioning.module.css'
 
 export default function NodeRestApiTestPage() {
     return (
         <div>
-            <Navbar highlight="projects"/>
+            <Navbar highlight="projects" />
+            <div className={styles.title}>User Authentication Demo - NodeJs</div>
             <h1 className={`
                 ${styles.heading}
                 ${animation.slide_in_left_small}
@@ -16,6 +18,11 @@ export default function NodeRestApiTestPage() {
                 ${styles.delay_subtext}
                 ${animation.slide_in_left_small}
             `}>Let's Test out how this works!</h3>
+            <div className={`
+                ${styles.expanding_divider}
+                ${position.vertical_center}
+                ${animation.expand_from_center}
+            `} />
         </div>
     )
 }
